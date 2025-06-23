f91614acc88578a532f2db3265d82d660d6fec8f
usama uddin <usamahafeez948@gmail.com>
2025-06-23 16:18:42 +0500

    Update server port and log message in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(1);
-  console.log();
+  await app.listen(4000);
+  console.log('Application is running on: http://localhost:4000');
 }
 bootstrap();
